ba0d3d28e038836c14ae70305a65e939fd8628c0
Stéphane Brunner <stephane.brunner@camptocamp.com>
2013-04-04 17:20:59 +0200

    Try to create an application ...

--- app-require.js
+++ app-require.js
@@ -5,3 +5,12 @@ goog.require('ol.RendererHints');
 goog.require('ol.View2D');
 goog.require('ol.layer.TileLayer');
 goog.require('ol.source.Stamen');
+
+
+goog.require('goog.dom');
+goog.require('goog.style');
+
+goog.require('goog.ui.TabPane');
+goog.require('goog.ui.TabPane.TabPage');
+goog.require('goog.ui.Textarea');
+
--- app.js
+++ app.js
@@ -20,3 +20,32 @@ var map = new ol.Map({
     zoom: 3
   })
 });
+
+
+var tabpane = new goog.ui.TabPane(goog.dom.getElement('tabs'));
+tabpane.addPage(new goog.ui.TabPane.TabPage(new goog.ui.Textarea().getElement(), 'p1'));
+tabpane.addPage(new goog.ui.TabPane.TabPage(new goog.ui.Textarea().getElement(), 'p2'));
+
+
+var header = goog.dom.getElement('header');
+var tabs = goog.dom.getElement('tabs');
+var toolbar = goog.dom.getElement('toolbar');
+var split = goog.dom.getElement('split');
+var statusbar = goog.dom.getElement('statusbar');
+var map = goog.dom.getElement('map');
+
+function resize() {
+    var header_height = goog.style.getBorderBoxSize(header).height;
+    var toolbar_height = goog.style.getBorderBoxSize(toolbar).height;
+    var tabs_width = goog.style.getBorderBoxSize(tabs).width;
+    var split_width = goog.style.getBorderBoxSize(split).width;
+    var statusbar_height = goog.style.getBorderBoxSize(statusbar).height;
+    var doc_size = goog.dom.getViewportSize();
+
+    goog.style.setWidth(map, doc_size.width - tabs_width - split_width);
+    goog.style.setHeight(map, doc_size.height - header_height - statusbar_height - toolbar_height);
+    goog.style.setWidth(toolbar, doc_size.width - tabs_width);
+    goog.style.setWidth(statusbar, doc_size.width - tabs_width);
+    goog.style.setHeight(tabs, doc_size.height - header_height);
+    goog.style.setHeight(split, doc_size.height - header_height - statusbar_height - toolbar_height);
+}
