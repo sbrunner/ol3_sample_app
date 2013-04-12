4af2f351948d73378dee3ee49ad6f77309e0c11b
Stéphane Brunner <stephane.brunner@camptocamp.com>
2013-04-12 12:24:46 +0200

    Add ol3 debugging comment

--- package.js
+++ package.js
@@ -1,4 +1,8 @@
 enyo.depends(
-    "ol.js",
-    "ol.css"
+    "ol.css",
+    "ol.js"
+    // for ol3 debugging replace 'ol.js' by the following lines
+//    'closure-library/closure/goog/base.js',
+//    'ol-deps.js',
+//    'app-require.js'
 )
